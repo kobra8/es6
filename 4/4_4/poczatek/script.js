20f79f96e3675b69921c3249187a12c530ddb21a
Jerzy Drobot <jerzy.drobot@mggp.com.pl>
2017-08-03 17:04:08 +0200

    Template literals exercise

--- 4/4_4/poczatek/script.js
+++ 4/4_4/poczatek/script.js
@@ -4,9 +4,19 @@ let giftCard = {
 	price: 50,
 	class: 'gift'
 }
-
+function createMark(product) {
+	// Znak backtick musi zaczynać się w lini razem z return
+	return `  
+	<div class="product ${product.class}">
+	<h3>${product.name}</h3>
+	<span>${product.sku}</span>
+	<span>${product.price}</span>
+	</div>
+	`
+}
 window.onload = function() {
-	
+	var content = document.querySelector(".content");
+	content.innerHTML = createMark(giftCard);	
 }
 
 
