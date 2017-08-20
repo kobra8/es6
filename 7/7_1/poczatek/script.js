ef72615e753c81f72e51093ed0980fe68c4d8dd1
kobra8 <jerzydrobot@gmail.com>
2017-08-20 21:35:38 +0200

    Iteration basic

--- 7/7_1/poczatek/script.js
+++ 7/7_1/poczatek/script.js
@@ -1,2 +1,15 @@
 var table = ['Adam', 'Tomek', 'Krzysiek', 'Pawel'];
 
+for ( let key in table ) {
+    console.log(key + " " + table[key]);
+}
+
+for ( let value of table) {
+    console.log(value);
+}
+
+const name = "Jerzy";
+
+for ( let char of name ) {
+    console.log(char);
+}
