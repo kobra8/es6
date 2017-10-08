97317d44bfc9d4a97953d04c273c9d9391662db5
kobra8 <jerzydrobot@gmail.com>
2017-10-08 21:49:28 +0200

    DOM plays

--- 2/es5functions/script.js
+++ 2/es5functions/script.js
@@ -1,7 +1,6 @@
 
 var findGreenBoxes = function () {
     boxes = document.getElementsByClassName("green");
-    console.log(boxes);
     return boxes
 }
 
@@ -13,6 +12,25 @@ var paintRed = function (boxes) {
     }
 }
 
+var paintGrey = function (boxes) {
+    var i = 0, max = boxes.length;
+    for (; i < max; i += 1) {
+        boxes[i].style.backgroundColor = "grey";
+    }
+}
+
+var paintBlack = function (boxes) {
+    var i = 0, max = boxes.length;
+    for (; i < max; i += 1) {
+        boxes[i].style.backgroundColor = "black";
+    }
+}
+var paintYellow = function (boxes) {
+    var i = 0, max = boxes.length;
+    for (; i < max; i += 1) {
+        boxes[i].style.backgroundColor = "yellow";
+    }
+}
 
 var findNodes = function (callback) {
     var i = 100,
@@ -39,5 +57,16 @@ var plusFife = function (node) {
     node = node + 5;
     console.log(node);
 }
+
+var word = document.getElementsByClassName("word");
+function gucio() {
+this.word[0].innerHTML="Gucio";
+}
+
+var duzoGuciow = function(src) {
+    var s = document.createElement("li");
+    s.innerHTML = src;
+    document.getElementsByTagName("ul")[0].appendChild(s);
+};
 
 findNodes(plusFife);
