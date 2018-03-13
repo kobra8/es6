7534256c8fd137f80001e2b7e1299459fe684a48
Jerzy Drobot <jerzy.drobot@mggp.com.pl>
2018-03-13 15:42:59 +0100

    This context

--- es6-second/app/js/index.js
+++ es6-second/app/js/index.js
@@ -41,5 +41,44 @@ console.log(citizen);
   }
   
   person.toString();
+
+  //Testing this
  
+  var a = {
+    name: "Hello a object",
+    doSomething: function() {
+      var self = this;
+      self.name = "Updated a object";
+      console.log(self);
+
+      var setName = function(newname) { // if You use Arrow function in this place You don`t need self variable
+        self.name = newname;
+      }
+      setName("Updated again! A object!");
+      console.log(self);
+    }
+  }
+
+  a.doSomething();
+
+  //Object properties short syntax ES6
+
+  const pricePropName = "price";
+
+  const calculator = (name, price) => { // calculator is const means readonly
+    
+    return {
+      name, // shorter version: name: name
+      add(n1,n2){ // shorter version: add: function(n1, n2)
+        return n1 + n2
+      },
+      [pricePropName+"Netto"]: price // You can use variable with [] to assign object property name
+    }
+  }
+ 
+  const calc = calculator("Casio", 19.99);
+
+  log(calc.name);
+  log(calc.add(20,10));
+  log(calc.priceNetto);
   
